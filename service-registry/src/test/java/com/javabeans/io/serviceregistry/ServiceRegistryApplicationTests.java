09960a2fc018cf9d0afcaa6e7b4f7a6bfa7d475e
Raja Gandharaw <raja.gandharaw@amadeus.com>
2021-04-19 22:52:15 +0530

    * Added Service Registry & Service Discovery

---
+++ service-registry/src/test/java/com/javabeans/io/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabeans.io.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
